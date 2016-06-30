929a82be03b76df8cb6ae82df2ab69050dc4b77d
waker.mahmud <waker.mahmud@gmail.com>
2016-07-01 02:00:06 +0600

    stand-alone web socket server
    stand-alone web socket server

---
+++ README.md
@@ -0,0 +1,126 @@
+gwsocket [![Build Status](https://travis-ci.org/allinurl/gwsocket.svg?branch=master)](http://travis-ci.org/allinurl/gwsocket) [![gwsocket](http://gwsocket.io/badge?v0.1)](http://gwsocket.io)
+========
+
+## What is it? ##
+**gwsocket** is a simple, standalone, language-agnostic, RFC6455 compliant
+WebSocket Server, written in C. It sits between your application and the
+client's browser, giving fast bidirectional communication between these two
+with ease and flexibility. More info at:
+[http://gwsocket.io](http://gwsocket.io/?src=gh).
+
+![gwsocket terminal](http://gwsocket.io/images/gwsocket-terminal-620.gif?2016050202)
+
+## How it Works? ##
+Very simple, just redirect the output from your application **(stdout)** to a
+file (named pipe) and let **gwsocket** transfer the data to the browser —
+That's it.
+
+For example, tailing your server's logs into the browser couldn't be easier
+
+    tail -f /var/log/nginx/access.log > /tmp/wspipein.fifo
+
+You can also get the client's data into **(stdin)** your application. In fact,
+you can even send your favorite ncurses program's output to the browser. See
+screencast above.
+
+## Features ##
+* Message Fragmentation per section [5.4](https://tools.ietf.org/html/rfc6455#page-33)
+* UTF-8 Handling
+* Framing (Text & Binary messages)
+* Multiplexed non-blocking network I/O
+* Ability to pipe data in/out in two different modes (**stdin/stdout & strict mode**)
+* Origin-based restriction
+* It passes the [Autobahn Testsuite](http://gwsocket.io/autobahn/) :)
+* and of course, [Valgrind](http://valgrind.org/) tested
+* missing something?, please feel free to post it on Github.
+
+## Why gwsocket? ##
+I needed a **fast**, **simple**, **no-dependencies**, **no libraries**,
+**RFC6455 compliant** WebSocket Server written in C that I could use for the
+upcoming version (v1.0) of [**GoAccess**](https://goaccess.io/) by simply
+piping data in and out — WebSockets made easy!
+
+## More Examples? ##
+gwsocket is language agnostic, look at the [Man Page](http://gwsocket.io/man?src=gh)
+for more details and examples on how to receive data from the browser and how
+to send it to the browser.
+
+## Installation ##
+Installing gwsocket is pretty easy. Just download, extract and compile it with:
+
+```
+$ wget http://tar.gwsocket.io/gwsocket-0.1.tar.gz
+$ tar -xzvf gwsocket-0.1.tar.gz
+$ cd gwsocket-0.1/
+$ ./configure
+$ make
+# make install
+```
+No dependencies needed. How nice isn't it :), well almost, you need `gcc`, `make`, etc.
+
+## Build from GitHub ##
+```
+$ git clone https://github.com/allinurl/gwsocket.git
+$ cd gwsocket
+$ autoreconf -fiv
+$ ./configure
+$ make
+# make install
+```
+
+## Data Modes ##
+In order to establish a channel between your application and the client's
+browser, gwsocket provides two methods that allow the user to send data in and
+out. The first one is through the use of the standard input (stdin), and the
+standard output (stdout). The second method is through a fixed-size header
+followed by the payload. See options below for more details.
+
+### STDIN/STDOUT ###
+The standard input/output is the simplest way of sending/receiving data to/from
+a client. However, it's limited to broadcasting messages to all clients. To
+send messages to or receive from a specific client, use the strict mode in the
+next section. See language specific examples [here](http://gwsocket.io/).
+
+### Strict Mode ###
+gwsocket implements its own tiny protocol for sending/receiving data. In
+contrast to the **stdin/stdout** mode, the strict mode allows you to
+send/receive data to/from specific connected clients as well as to keep track
+of who opened/closed a WebSocket connection. It also gives you the ability to
+pack and send as much data as you would like on a single message. See language
+specific examples [here](http://gwsocket.io/).
+
+## Command Line / Config Options ##
+The following options can be supplied to the command line.
+
+
+| Command Line Option          | Description                                                         |
+| ---------------------------- | --------------------------------------------------------------------|
+| `-p --port`                  | Specifies the port to bind.                                         |
+| `-h --help`                  | Command line help.                                                  |
+| `-V --version`               | Display version information and exit.                               |
+| `--access-log=<path/file>`   | Specifies the path/file for the access log.                         |
+| `--bind=<addr>`              | Specifies the address to bind.                                      |
+| `--echo-mode`                | Set the server to echo all received messages.                       |
+| `--max-frame-size=<bytes>`   | Maximum size of a websocket frame.                                  |
+| `--origin=<origin>`          | Ensure clients send the specified origin header upon handshake.     |
+| `--pipein=<path/file>`       | Creates a named pipe (FIFO) that reads from on the given path/file. |
+| `--pipeout=<path/file>`      | Creates a named pipe (FIFO) that writes to the given path/file.     |
+| `--strict`                   | Parse messages using strict mode. See man page for more details.    |
+
+## Roadmap ##
+* Support for `epoll` and `kqueue`
+* Support for SSL
+* Add more command line options
+* Add configuration file
+* Please feel free to open an issue to discuss a new feature.
+
+## License ##
+MIT Licensed
+
+## Contributing ##
+
+Any help on gwsocket is welcome. The most helpful way is to try it out and give
+feedback. Feel free to use the Github issue tracker and pull requests to
+discuss and submit code changes.
+
+Enjoy!
---
+++ src/gwsocket.c
@@ -0,0 +1,274 @@
+/**
+ * gwsocket.c -- An rfc6455-complaint Web Socket Server
+ *    _______       _______            __        __
+ *   / ____/ |     / / ___/____  _____/ /_____  / /_
+ *  / / __ | | /| / /\__ \/ __ \/ ___/ //_/ _ \/ __/
+ * / /_/ / | |/ |/ /___/ / /_/ / /__/ ,< /  __/ /_
+ * \____/  |__/|__//____/\____/\___/_/|_|\___/\__/
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2009-2016 Gerardo Orellana <hello @ goaccess.io>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include "gwsocket.h"
+#include "log.h"
+#include "websocket.h"
+#include "xmalloc.h"
+
+static WSServer *server = NULL;
+
+/* *INDENT-OFF* */
+static char short_options[] = "p:Vh";
+static struct option long_opts[] = {
+  {"port"           , required_argument , 0 , 'p' } ,
+  {"addr"           , required_argument , 0 ,  0  } ,
+  {"echo-mode"      , no_argument       , 0 ,  0  } ,
+  {"max-frame-size" , required_argument , 0 ,  0  } ,
+  {"origin"         , required_argument , 0 ,  0  } ,
+  {"pipein"         , required_argument , 0 ,  0  } ,
+  {"pipeout"        , required_argument , 0 ,  0  } ,
+  {"access-log"     , required_argument , 0 ,  0  } ,
+  {"strict"         , no_argument       , 0 ,  0  } ,
+  {"version"        , no_argument       , 0 , 'V' } ,
+  {"help"           , no_argument       , 0 , 'h' } ,
+  {0, 0, 0, 0}
+};
+
+/* Command line help. */
+static void
+cmd_help (void)
+{
+  printf ("\nGWSocket - %s\n\n", GW_VERSION);
+
+  printf (
+  "Usage: "
+  "gwsocket [ options ... ] -p [--addr][--origin][...]\n"
+  "The following options can also be supplied to the command:\n\n"
+  ""
+  "  -p --port=<port>         - Specifies the port to bind.\n"
+  "  -h --help                - This help.\n"
+  "  -V --version             - Display version information and exit.\n"
+  "  --access-log=<path/file> - Specifies the path/file for the access log.\n"
+  "  --addr=<addr>            - Specify an IP address to bind to.\n"
+  "  --echo-mode              - Echo all received messages.\n"
+  "  --max-frame-size=<bytes> - Maximum size of a websocket frame. This\n"
+  "                             includes received frames from the client\n"
+  "                             and messages through the named pipe.\n"
+  "  --origin=<origin>        - Ensure clients send the specified origin\n"
+  "                             header upon the WebSocket handshake.\n"
+  "  --pipein=<path/file>     - Creates a named pipe (FIFO) that reads\n"
+  "                             from on the given path/file.\n"
+  "  --pipeout=<path/file>    - Creates a named pipe (FIFO) that writes\n"
+  "                             to on the given path/file.\n"
+  "  --strict                 - Parse messages using strict mode. See\n"
+  "                             man page for more details.\n"
+  "\n"
+  "See the man page for more information `man gwsocket`.\n\n"
+  "For more details visit: http://gwsocket.io\n"
+  "gwsocket Copyright (C) 2016 by Gerardo Orellana"
+  "\n\n"
+  );
+  ws_stop (server);
+  exit (EXIT_FAILURE);
+}
+/* *INDENT-ON* */
+
+static void
+handle_signal_action (int sig_number)
+{
+  if (sig_number == SIGINT) {
+    printf ("SIGINT caught!\n");
+    /* if it fails to write, force stop */
+    if ((write (server->self_pipe[1], "x", 1)) == -1 && errno != EAGAIN)
+      ws_stop (server);
+  } else if (sig_number == SIGPIPE) {
+    printf ("SIGPIPE caught!\n");
+  }
+}
+
+static int
+setup_signals (void)
+{
+  struct sigaction sa;
+  memset (&sa, 0, sizeof (sa));
+  sa.sa_handler = handle_signal_action;
+  if (sigaction (SIGINT, &sa, 0) != 0) {
+    perror ("sigaction()");
+    return -1;
+  }
+  if (sigaction (SIGPIPE, &sa, 0) != 0) {
+    perror ("sigaction()");
+    return -1;
+  }
+  return 0;
+}
+
+static int
+onopen (WSPipeOut * pipeout, WSClient * client)
+{
+  uint32_t hsize = sizeof (uint32_t) * 3;
+  char *hdr = calloc (hsize, sizeof (char));
+  char *ptr = hdr;
+
+  ptr += pack_uint32 (ptr, client->listener);
+  ptr += pack_uint32 (ptr, 0x10);
+  ptr += pack_uint32 (ptr, INET6_ADDRSTRLEN);
+
+  ws_write_fifo (pipeout, hdr, hsize);
+  ws_write_fifo (pipeout, client->remote_ip, INET6_ADDRSTRLEN);
+  free (hdr);
+
+  return 0;
+}
+
+static int
+onclose (WSPipeOut * pipeout, WSClient * client)
+{
+  uint32_t hsize = sizeof (uint32_t) * 3;
+  char *hdr = calloc (hsize, sizeof (char));
+  char *ptr = hdr;
+
+  ptr += pack_uint32 (ptr, client->listener);
+  ptr += pack_uint32 (ptr, 0x08);
+  ptr += pack_uint32 (ptr, 0);
+
+  ws_write_fifo (pipeout, hdr, hsize);
+  free (hdr);
+
+  return 0;
+}
+
+static int
+onmessage (WSPipeOut * pipeout, WSClient * client)
+{
+  WSMessage **msg = &client->message;
+  uint32_t hsize = sizeof (uint32_t) * 3;
+  char *hdr = NULL, *ptr = NULL;
+
+  hdr = calloc (hsize, sizeof (char));
+  ptr = hdr;
+  ptr += pack_uint32 (ptr, client->listener);
+  ptr += pack_uint32 (ptr, (*msg)->opcode);
+  ptr += pack_uint32 (ptr, (*msg)->payloadsz);
+
+  ws_write_fifo (pipeout, hdr, hsize);
+  ws_write_fifo (pipeout, (*msg)->payload, (*msg)->payloadsz);
+  free (hdr);
+
+  return 0;
+}
+
+static void
+parse_long_opt (const char *name, const char *oarg)
+{
+  if (!strcmp ("echo-mode", name))
+    ws_set_config_echomode (1);
+  if (!strcmp ("max-frame-size", name))
+    ws_set_config_frame_size (atoi (oarg));
+  if (!strcmp ("origin", name))
+    ws_set_config_origin (oarg);
+  if (!strcmp ("pipein", name))
+    ws_set_config_pipein (oarg);
+  if (!strcmp ("pipeout", name))
+    ws_set_config_pipeout (oarg);
+  if (!strcmp ("strict", name))
+    ws_set_config_strict (1);
+  if (!strcmp ("access-log", name))
+    ws_set_config_accesslog (oarg);
+}
+
+/* Read the user's supplied command line options. */
+static int
+read_option_args (int argc, char **argv)
+{
+  int o, idx = 0;
+
+  while ((o = getopt_long (argc, argv, short_options, long_opts, &idx)) >= 0) {
+    if (-1 == o || EOF == o)
+      break;
+    switch (o) {
+    case 'p':
+      ws_set_config_port (optarg);
+      break;
+    case 'h':
+      cmd_help ();
+      return 1;
+    case 'V':
+      fprintf (stdout, "GWSocket %s\n", GW_VERSION);
+      return 1;
+    case 0:
+      parse_long_opt (long_opts[idx].name, optarg);
+      break;
+    case '?':
+      return 1;
+    default:
+      return 1;
+    }
+  }
+
+  for (idx = optind; idx < argc; idx++)
+    cmd_help ();
+
+  return 0;
+}
+
+static void
+set_self_pipe (void)
+{
+  /* Initialize self pipe. */
+  if (pipe (server->self_pipe) == -1)
+    FATAL ("Unable to create pipe: %s.", strerror (errno));
+
+  /* make the read and write pipe non-blocking */
+  set_nonblocking (server->self_pipe[0]);
+  set_nonblocking (server->self_pipe[1]);
+}
+
+int
+main (int argc, char **argv)
+{
+  if ((server = ws_init ("0.0.0.0", "7890")) == NULL) {
+    perror ("Error during ws_init.\n");
+    exit (EXIT_FAILURE);
+  }
+  /* callbacks */
+  server->onclose = onclose;
+  server->onmessage = onmessage;
+  server->onopen = onopen;
+
+  set_self_pipe ();
+  if (setup_signals () != 0)
+    exit (EXIT_FAILURE);
+
+  if (read_option_args (argc, argv) == 0)
+    ws_start (server);
+  ws_stop (server);
+
+  return EXIT_SUCCESS;
+}
